a2ca096a1751c559dc96bb49a759a020e2e0870f
RenBas <renbas9992000@gmail.com>
2026-07-10 16:43:12 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,36 +1,28 @@
-"""
-Utility package for the SBM Dashboard.
-"""
+"""Utilities package for SBM Dashboard."""
 
-from .constants import DIMENSION_NAMES, DEGREE_COLORS, SHIELD_COLORS, INDICATORS
-from .data_loader import load_sdo_data, load_all_schools, get_schools_by_sdo, compute_dimension_averages
+from .constants import DIMENSION_NAMES, SHIELD_COLORS, DEGREE_COLORS
+from .data_loader import (
+    load_sdo_data,
+    load_all_schools,
+    get_schools_by_sdo,
+    compute_dimension_averages
+)
 from .map_helpers import add_sdo_shield, add_school_dot
-from .chart_helpers import create_radar_chart, create_trend_chart, create_indicators_table
-from .auth import authenticate, login_status, logout, get_accessible_schools, get_accessible_divisions_summary, is_school_head
-from .download_helpers import generate_report_data, generate_template_csv
+from .chart_helpers import (
+    create_radar_chart,
+    create_trend_chart,
+    create_indicators_table,
+    render_school_dashboard
+)
+from .auth import (
+    authenticate,
+    login_status,
+    logout,
+    get_accessible_schools,
+    get_accessible_divisions_summary,
+    is_school_head
+)
+from .download_helpers import generate_report_data, generate_excel_template
+from .export_helpers import generate_excel_report, generate_pdf_report
 from .synopsis_generator import generate_synopsis
-
-__all__ = [
-    "DIMENSION_NAMES",
-    "DEGREE_COLORS",
-    "SHIELD_COLORS",
-    "INDICATORS",
-    "load_sdo_data",
-    "load_all_schools",
-    "get_schools_by_sdo",
-    "compute_dimension_averages",
-    "add_sdo_shield",
-    "add_school_dot",
-    "create_radar_chart",
-    "create_trend_chart",
-    "create_indicators_table",
-    "authenticate",
-    "login_status",
-    "logout",
-    "get_accessible_schools",
-    "get_accessible_divisions_summary",
-    "is_school_head",
-    "generate_report_data",
-    "generate_template_csv",
-    "generate_synopsis",
-]
+from .twin_ui import render_sandbox
